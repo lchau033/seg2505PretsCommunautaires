753ce8eef359af3fd7bcaab1d890b2636241c5ee
krazygloue <joesimpleplan@hotmail.com>
2014-11-06 11:12:20 -0500

    Update AjouterObjetActivity.java

--- ObjetPret/src/ca/uottawa/eecs/seg2505/objetpret/AjouterObjetActivity.java
+++ ObjetPret/src/ca/uottawa/eecs/seg2505/objetpret/AjouterObjetActivity.java
@@ -11,6 +11,7 @@ public class AjouterObjetActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_ajouter_objet);
+		getActionBar().setTitle("Ajouter un objet");
 	}
 
 	@Override
