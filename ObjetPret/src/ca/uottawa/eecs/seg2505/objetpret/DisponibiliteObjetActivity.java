5e6c0e4e18a986022347096e5d933f65ec39a195
lchau033 <lchau033@uottawa.ca>
2014-11-12 23:32:25 -0500

    Update DisponibiliteObjetActivity.java

--- ObjetPret/src/ca/uottawa/eecs/seg2505/objetpret/DisponibiliteObjetActivity.java
+++ ObjetPret/src/ca/uottawa/eecs/seg2505/objetpret/DisponibiliteObjetActivity.java
@@ -1,22 +1,26 @@
 package ca.uottawa.eecs.seg2505.objetpret;
 
 import android.support.v7.app.ActionBarActivity;
+import android.app.AlertDialog;
+import android.widget.RadioButton;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class DisponibiliteObjetActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_disponibilite_objet);
+		setContentView(R.layout.activity_main);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.disponibilite_objet, menu);
+		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
@@ -30,5 +34,70 @@ public class DisponibiliteObjetActivity extends ActionBarActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	public void confirmDisponibility(){
+		DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+		    @Override
+		    public void onClick(DialogInterface dialog, int which) {
+		        switch (which){
+		        case DialogInterface.BUTTON_POSITIVE:
+		            //Yes button clicked
+		            break;
+
+		        case DialogInterface.BUTTON_NEGATIVE:
+		            //No button clicked
+		            break;
+		        }
+		    }
+		};
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage("Êtes vous sure de vouloir changer la disponibilité de l'objet?").setPositiveButton("Yes", dialogClickListener)
+		    .setNegativeButton("No", dialogClickListener).show();
+	}
+	
+	public void annulerLesChangements(){
+		DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+		    @Override
+		    public void onClick(DialogInterface dialog, int which) {
+		        switch (which){
+		        case DialogInterface.BUTTON_POSITIVE:
+		            //Yes button clicked
+		            break;
+
+		        case DialogInterface.BUTTON_NEGATIVE:
+		            //No button clicked
+		            break;
+		        }
+		    }
+		};
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage("Êtes vous sure de vouloir annuler ces changements").setPositiveButton("Yes", dialogClickListener)
+		    .setNegativeButton("No", dialogClickListener).show();
+	}
+	public void onRadioButtonClicked(View view) {
+	    // Is the button now checked?
+	    boolean checked = ((RadioButton) view).isChecked();
+	    
+	    // Check which radio button was clicked
+	    switch(view.getId()) {
+	        case R.id.en_demande_rbutton:
+	            if (checked)
+	                // en demande
+	            break;
+	        case R.id.disponible_rbutton:
+	            if (checked)
+	                // disponible
+	            break;
+	        case R.id.non_disponible_rbutton:
+	        	if(checked)
+	        		// non-disponible
+        		break;
+	        case R.id.prete_rbutton:
+	        	if(checked)
+	        		// prete
+        		break;
+	    }
 	}
 }
